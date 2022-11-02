910c7ae10aa7a5504b0cf4f0c17af5743b7cca1f
gmun <garrettohrt@gmail.com>
2022-11-01 22:25:12 -0500

    rewrite renderLicenseSection

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -17,52 +17,50 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (license === "Apache License 2.0") {
-    return `
-(https://www.apache.org/licenses/LICENSE-2.0.txt).
-    `;
-  } else if (license === "GNU GPLv3") {
-    return `
-(https://www.gnu.org/licenses/gpl-3.0.en.html).
-    `;
-  } else if (license === "BSD 3-Clause License") {
-    return `
-(https://opensource.org/licenses/BSD-3-Clause).
-    `;
-  } else if (license === "MIT License") {
-    return `
-(https://opensource.org/licenses/MIT).
-    `;
-  } else {
-    return ""
-  };
-}
+// function renderLicenseLink(license) {
+//   if (license === "Apache License 2.0") {
+//     return `
+//     * [License](#license)
+//     `;
+//   } else if (license === "GNU GPLv3") {
+//     return `
+//     * [License](#license)
+//     `;
+//   } else if (license === "BSD 3-Clause License") {
+//     return `
+//     * [License](#license)
+//     `;
+//   } else if (license === "MIT License") {
+//     return `
+//     * [License](#license)
+//     `;
+//   } else {
+//     return ""
+//   };
+// }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
+  console.log("expect license", license)
   if (license === "Apache License 2.0") {
     return `## License\n
-    This application is covered by the Apache v2.0 License, the terms of which can be found [here]${renderLicenseLink(data.license)}
+    This application is covered by the ${license}
     `;
   } else if
     (license === "GNU GPLv3") {
     return `## License\n
-    This application is covered by the GNU General Purpose License, the terms of which can be found [here]
-    ${renderLicenseLink(data.license)}
+    This application is covered by the ${license}
     `;
   } else if
     (license === "BSD 3-Clause License") {
     return `## License\n
-    This application is covered by the BSD 3-Clause License, the terms of which can be found [here]
-    ${renderLicenseLink(data.license)}
+    This application is covered by the ${license}
     `;
   } else if
     (license === "MIT License") {
     return `## License\n
-    This application is covered by the MIT License, the terms of which can be found [here]
-    ${renderLicenseLink(data.license)}
+    This application is covered by the ${license}
     `;
   } else {
     return ""
